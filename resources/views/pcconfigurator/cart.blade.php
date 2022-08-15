d30f456238f1ea41fe45101053e8bff49dbd410c
Denis6902 <bata.student@skolabaltaci.cz>
2022-08-15 17:52:19 +0200

    Creating card blade.

---
+++ resources/views/pcconfigurator/cart.blade.php
@@ -0,0 +1,40 @@
+@extends("app")
+
+@section("content")
+    @include("header")
+    <h1>Váš nákupní košík</h1>
+
+    <main class="cart-things">
+        <div class="cart-body">
+            <p>img</p>
+            <p>id</p>
+            <p>name</p>
+            <p>price</p>
+        </div>
+        <div class="cart-body">
+            <p>img</p>
+            <p>id</p>
+            <p>name</p>
+            <p>price</p>
+        </div>
+        <div class="cart-body">
+            <p>img</p>
+            <p>id</p>
+            <p>name</p>
+            <p>price</p>
+        </div>
+        <div class="cart-body">
+            <p>img</p>
+            <p>id</p>
+            <p>name</p>
+            <p>price</p>
+        </div>
+        <div class="cart-body">
+            <p>img</p>
+            <p>id</p>
+            <p>name</p>
+            <p>price</p>
+        </div>
+    </main>
+    @include("footer")
+@endsection
