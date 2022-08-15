12eb8cc3db31491fe1b43a4a0568e311294ca20e
Denis6902 <bata.student@skolabaltaci.cz>
2022-08-15 16:38:12 +0200

    Removing old way for changing theme.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,24 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @if(\Illuminate\Support\Facades\Session::get('theme') == 'dark')
-        <link href="/bootstrap-night.min.css" rel="stylesheet">
-    @else
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-              crossorigin="anonymous">
-    @endif
+    <meta name="color-scheme" content="light dark">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-nightshade.min.css" rel="stylesheet">
     <link href="{{ URL::asset('main.css') }}" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/js/darkmode.min.js"></script>
     <title>PC Konfigurátor</title>
 </head>
 <body class="d-flex flex-column min-vh-100">
 {{--TODO: okomentovat CELÝ KOD...--}}
-@php
-    \Illuminate\Support\Facades\Session::put("currentRoute", \Illuminate\Support\Facades\Request::fullUrl())
-@endphp
 @yield("content")
 </body>
 </html>
