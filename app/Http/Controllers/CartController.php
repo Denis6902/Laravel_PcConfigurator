e89c11cc2380478e592defd236ccf7f3b10ec60e
Denis6902 <bata.student@skolabaltaci.cz>
2022-08-16 17:59:07 +0200

    Adding showing components in cart on cart.blade.php and CartController.php.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,15 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CPU;
+use App\Models\CpuCooler;
+use App\Models\GPU;
+use App\Models\Memory;
+use App\Models\Monitor;
+use App\Models\Motherboard;
+use App\Models\OS;
+use App\Models\PcCase;
+use App\Models\PSU;
+use App\Models\Storage;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Session;
 
 class CartController extends BaseController
 {
     function read()
     {
-        return view('pcconfigurator.cart', [
+        $cpuId = Session::get('cpu');
+        $cpuCoolerId = Session::get('cpucooler');
+        $motherboardId = Session::get('motherboard');
+        $memoryId = Session::get('memory');
+        $storageId = Session::get('storage');
+        $gpuId = Session::get('gpu');
+        $pcCaseId = Session::get('pccase');
+        $psuId = Session::get('psu');
+        $osId = Session::get('os');
+        $monitorId = Session::get('monitor');
 
+        $cpu = CPU::Find($cpuId);
+        $cpuCooler = CpuCooler::Find($cpuCoolerId);
+        $motherboard = Motherboard::Find($motherboardId);
+        $memory = Memory::Find($memoryId);
+        $storage = Storage::Find($storageId);
+        $gpu = GPU::Find($gpuId);
+        $pcCase = PcCase::Find($pcCaseId);
+        $psu = PSU::Find($psuId);
+        $os = OS::Find($osId);
+        $monitor = Monitor::Find($monitorId);
+
+        $products = new Collection();
+        $products->add($cpu);
+        $products->add($cpuCooler);
+        $products->add($motherboard);
+        $products->add($memory);
+        $products->add($storage);
+        $products->add($gpu);
+        $products->add($pcCase);
+        $products->add($psu);
+        $products->add($os);
+        $products->add($monitor);
+
+        $filtered_products = $products->filter(function ($value, $key) {
+            return $value != null;
+        });
+
+
+        return view('pcconfigurator.cart', [
+            "cpu" => $cpu,
+            "cpuCooler" => $cpuCooler,
+            "motherboard" => $motherboard,
+            "memory" => $memory,
+            "storage" => $storage,
+            "gpu" => $gpu,
+            "pcCase" => $pcCase,
+            "psu" => $psu,
+            "os" => $os,
+            "monitor" => $monitor,
+            "products" => $filtered_products,
         ]);
     }
 }
--- resources/views/pcconfigurator/cart.blade.php
+++ resources/views/pcconfigurator/cart.blade.php
@@ -2,39 +2,33 @@
 
 @section("content")
     @include("header")
-    <h1>Váš nákupní košík</h1>
+    @if(\Illuminate\Support\Facades\Session::get('totalPrice') == "0")
+        <h1>Nemáš nic v košíku...</h1>
+    @else
+        <h1>Váš nákupní košík</h1>
+    @endif
 
     <main class="cart-things">
-        <div class="cart-body">
-            <p>img</p>
-            <p>id</p>
-            <p>name</p>
-            <p>price</p>
-        </div>
-        <div class="cart-body">
-            <p>img</p>
-            <p>id</p>
-            <p>name</p>
-            <p>price</p>
-        </div>
-        <div class="cart-body">
-            <p>img</p>
-            <p>id</p>
-            <p>name</p>
-            <p>price</p>
-        </div>
-        <div class="cart-body">
-            <p>img</p>
-            <p>id</p>
-            <p>name</p>
-            <p>price</p>
-        </div>
-        <div class="cart-body">
-            <p>img</p>
-            <p>id</p>
-            <p>name</p>
-            <p>price</p>
-        </div>
+        @foreach($products as $product)
+            <div class="cart-body">
+                <img class="cart-image"
+                     src="{{ \App\Models\IllustrationImage::Find($product->illustration_image_id)->src }}"
+                     alt="{{ \App\Models\IllustrationImage::Find($product->illustration_image_id)->alt }}">
+                <div>
+                    <a href='{{route('product', ['product'=>"$product->tableName", 'id'=>$product->id])}}'>
+                        {{$product->name}}</a>
+                    <p>{{$product->id}}</p>
+                </div>
+
+                <div class="cart-cancel">
+                    <p>{{$product->price}} Kč</p>
+                    <a class="btn btn-sm btn-outline-primary"
+                       href="{{route('deleteProduct', ['product'=>"$product->tableName"])}}">
+                        <img style="width: 10px" src="/./img/cancel.svg" alt="Cancel">
+                    </a>
+                </div>
+            </div>
+        @endforeach
     </main>
     @include("footer")
 @endsection
