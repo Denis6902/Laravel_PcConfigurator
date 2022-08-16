be277331a87b55c6fd61bfa892dd6a92435603f3
Denis6902 <bata.student@skolabaltaci.cz>
2022-08-16 17:57:41 +0200

    Updating about blade.

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -3,36 +3,15 @@
 @section("content")
     @include("header")
     <main>
-        <h1><a target="_blank" href="https://github.com/Denis6902/Laravel_Aplikace">Odkaz na GitHub</a></h1>
-        <h2 id="aplikace-v-php-frameworku-laravel">Aplikace v PHP frameworku Laravel</h2>
+        <h1><a href="{{route("oldAbout")}}">Staré zadání úkolu</a></h1>
+        <h1><a target="_blank" href="https://github.com/Denis6902/Laravel_PcConfigurator/tree/v2">Odkaz na GitHub</a></h1>
+        <h2 id="aplikace-v-php-frameworku-laravel">Aplikace v PHP frameworku Laravel v2</h2>
         <h3 id="body-zad-n-">Body zadání:</h3>
         <ul>
-            <li>Navrhnout funkční administrační prostředí pro libovolnou aplikaci nebo pro fakturační systém případně
-                konfigurátor PC
-                sestav.
+            <li>
+                Vylepšit staré zadání...
             </li>
-            <li>Struktura DB spolu s migračními třídami, factory a seedery.</li>
-            <li>Vytvořit modely pro správnou funkčnost.</li>
-            <li>Vytvořit view pro všechny operace CRUD + využít layout templates a pomocných nástrojů BLADE.</li>
-            <li>Vytvořit controllery pro ovládání administrace</li>
-            <li>Vytvořit potřebné routy pro směrování uvnitř administrace</li>
-            <li>Dodržet model MVC.</li>
         </ul>
-        <h3 id="nutn-n-le-itosti-">Nutné náležitosti:</h3>
-        <ul>
-            <li>Minimální počet tabulek je (6)</li>
-            <li>Minimálně 2 tabulky bude mít vazbu 1:N (N:1) pokud bude možnost využít vazbu N:N tak ji využít</li>
-            <li>Vytvořit ukázkový seeder pro vložení minimálně 5 záznamů do každé tabulky</li>
-            <li>Vytvořit design pomocí vlastního CSS, Bootstrap nebo jiných knihoven</li>
-            <li>Každá část v administraci bude mít kompletní CRUD operace (Create, Read, Update, Delete)</li>
-            <li>Budou existovat všechny View pro všechny CRUD operace</li>
-        </ul>
-        <h3 id="bonus">Bonus</h3>
-        <ul>
-            <li>Přihlašování a registrace uživatelů (v případě bonusu řešetě jako poslední)</li>
-            <li>Implementace Soft Delete pro mazání skrze &quot;tabulkový koš&quot;</li>
-        </ul>
-
     </main>
     @include("footer")
 @endsection
